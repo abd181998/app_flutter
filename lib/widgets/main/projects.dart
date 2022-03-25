9b656f04984101012262f129c9f07aa564e14717
abd181998 <mahgouba4@gmail.com>
2022-03-25 18:03:00 +0200

    Initial commit

---
+++ lib/widgets/main/projects.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:provider_flutter/widgets/main/project_card.dart';
+
+import '../../constants.dart';
+import '../../models/projects.dart';
+import '../../responsive.dart';
+
+
+class Projects extends StatelessWidget {
+  const Projects({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Our Projects',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        Responsive(
+          desktop: buildGridView(
+            itemCount: demoProjects.length,
+            crossAxsitCount: 3,
+            childAspectRatio: 0.75,
+            itemBuilder: (context, index) =>
+                ProjectCard(project: demoProjects[index]),
+          ),
+          tablet: buildGridView(
+            itemCount: demoProjects.length,
+            crossAxsitCount: MediaQuery.of(context).size.width < 900 ? 2 : 3,
+            childAspectRatio: 0.75,
+            itemBuilder: (context, index) =>
+                ProjectCard(project: demoProjects[index]),
+          ),
+          mobileLarge: buildGridView(
+            itemCount: demoProjects.length,
+            crossAxsitCount: 2,
+            childAspectRatio: 0.75,
+            itemBuilder: (context, index) =>
+                ProjectCard(project: demoProjects[index]),
+          ),
+          mobile: buildGridView(
+            itemCount: demoProjects.length,
+            crossAxsitCount: 1,
+            childAspectRatio: 0.75,
+            itemBuilder: (context, index) =>
+                ProjectCard(project: demoProjects[index]),
+          ),
+        )
+      ],
+    );
+  }
+
+  GridView buildGridView({
+    required int itemCount,
+    required int crossAxsitCount,
+    required double childAspectRatio,
+    required IndexedWidgetBuilder itemBuilder,
+  }) {
+    return GridView.builder(
+        shrinkWrap: true,
+        itemCount: itemCount,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: crossAxsitCount,
+          childAspectRatio: childAspectRatio,
+          crossAxisSpacing: kDefaultPadding,
+          mainAxisSpacing: kDefaultPadding,
+        ),
+        itemBuilder: itemBuilder);
+  }
+}
